a7da3daed8c8b0ace0b45460f7979c9ddfa0fbfe
Michael Gill <michael8123@outlook.com>
2018-07-06 11:43:02 -0700

    added parseable commands

---
+++ commandparser.py
@@ -0,0 +1,129 @@
+"""
+commandparser.py: process commands.
+this will eventually have support for pipes, but not yet.
+"""
+
+__author__ = 'Michael Gill <michaelveenstra12@gmail.com>'
+__version__ = '0.0a'
+__all__ = ['parse']
+
+import re
+import commands
+import os
+import subprocess
+
+PATHEXT = os.environ['pathext'].split(';')
+PATH = os.environ['path'].split(';') + [os.getcwd()]
+
+
+def _rm_ext(fname):
+    d = fname.split('.')[:-1]
+    return '.'.join(d)
+
+
+def _get_path():
+    d = []
+    for path in PATH:
+        if os.path.isdir(path):
+            d += os.listdir(path)
+        else:
+            d += [path]
+    # print(len(d))
+    # print(tuple(os.getenv('pathext').split(';')))
+    return d
+
+
+def _get_path_from_str(cmd):
+    """
+    find the path for the executable given along path.
+    """
+    cmd = cmd.upper()
+    for path in PATH:
+        if os.path.isdir(path):
+            for i in os.listdir(path):
+                i = i.upper()
+                if _rm_ext(i) == _rm_ext(cmd) and i.endswith(tuple(PATHEXT)):
+                    return os.path.join(path, i).lower()
+
+        else:
+            if path.upper().split('\\')[-1].startswith(cmd) and \
+              path.upper().split('\\')[-1].endswith(tuple(PATHEXT)):
+                return path
+
+
+PATH_CMDS = list(
+    filter(
+        lambda x: x.upper().endswith(tuple(os.getenv('pathext').split(';'))),
+        _get_path()))
+ALL_CMDS = commands.__all__ + PATH_CMDS
+
+
+class _PathCmds:
+    base = PATH_CMDS
+    def __contains__(self, item):
+        item = item.upper()
+        for i in PATHEXT:
+            if item + i in self.base or item in self.base:
+                return True
+        return False
+
+    def __iter__(self):
+        for i in self.base:
+            yield i
+
+    def __getitem__(self, item):
+        item = item.upper()
+        for i in PATHEXT:
+            if item + i in self.base:
+                print(item + i)
+                return item + i
+
+            elif item in i:
+                print(item)
+                return item
+
+
+PATH_CMDS = _PathCmds()
+
+
+class _ExecutableCommand:
+    def __init__(self, path, args=()):
+        self.path = path
+        self.args = args
+
+    def __call__(self):
+        return subprocess.call([self.path] + list(self.args))
+
+
+class CommandParser:
+    def __init__(self, cmdstr):
+        self.cmdstr = cmdstr
+        self.args = self.rm_strings()
+
+    def rm_strings(self):
+        stringsplit = re.split('(".*")', self.cmdstr)
+        lst = []
+        for i in stringsplit:
+            lst += i.split() if not re.match('".*"', i) else [i.strip('""')]
+
+        return lst
+
+    def get_program(self):
+        # print(self.args[0])
+        cmd = None
+
+        if self.args[0] in commands.__all__:
+            # built-in command
+            # print('found command in commands.__all__')
+            cmd_f = getattr(commands, self.args[0])
+            cmd = lambda: cmd_f(self.args[1:])
+
+        elif self.args[0] in PATH_CMDS:
+            cmd = _ExecutableCommand(self.args[0])
+
+        if cmd is None:
+            raise TypeError('Command ')
+        return cmd
+
+
+print(PATH_CMDS['cmd'])
---
+++ lsutils.py
@@ -0,0 +1,156 @@
+"""
+lsutils.py- i did this to have commands.py more readable.
+"""
+
+import os
+
+import colorama as cm
+
+cm.init()
+
+_LS_COLOURS = {
+    'broken-symlink': (cm.Fore.BLACK, cm.Back.RED),
+    'symlink':        (cm.Fore.CYAN, cm.Back.BLACK),
+    'directory':      (cm.Fore.BLUE, cm.Back.BLACK),
+    'archive':        (cm.Fore.RED, cm.Back.BLACK),
+    'other':          (cm.Fore.WHITE, cm.Back.BLACK),
+    'shortcut':       (cm.Fore.MAGENTA, cm.Back.BLACK),
+    'executable':     (cm.Fore.GREEN, cm.Back.BLACK),
+}
+
+PRIORITIES = ['broken-symlink', 'directory', 'symlink', 'shortcut',
+'executable', 'other']
+
+_EXECUTABLE_EXTENSIONS = os.environ.get('PATHEXT')\
+                         .lower().strip('.').split(';.')\
+                         or ['exe', 'com', 'bat', 'cmd']
+
+_ARCHIVE_EXTENSIONS = [
+    'tar', 'zip', 'gz', 'tgz', '7z', 'rar', 'xz', 'lzma',
+    'iso', 'bz2', 'bz', 'dmg', 'cab', 'jar', 'pyz'
+]
+
+def _get_coloured_str(filename):
+    """
+    returns the proper string, padded by proper
+    type_s on the file type.
+    """
+
+    if os.path.islink(filename):
+        try:
+            os.stat(filename)
+            colour = _LS_COLOURS['symlink']
+        except OSError:
+            # the link is broken
+            colour = _LS_COLOURS['broken-symlink']
+
+    elif os.path.isdir(filename):
+        colour = _LS_COLOURS['directory']
+
+    else:
+        # we are now relying on the extension
+        try:
+            extension = filename.split('.')[-1]
+        except IndexError:
+            # file has no extension
+            extension = None
+            colour = _LS_COLOURS['other']
+
+        if extension is not None:
+            if extension in _ARCHIVE_EXTENSIONS:
+                colour = _LS_COLOURS['archive']
+
+            elif extension in _EXECUTABLE_EXTENSIONS:
+                colour = _LS_COLOURS['executable']
+
+            elif extension == 'lnk':
+                colour = _LS_COLOURS['shortcut']
+
+            else:
+                colour = _LS_COLOURS['other']
+
+    return ''.join(colour + (filename.split('\\')[-1], cm.Style.RESET_ALL))
+
+class _LsItem:
+    """
+    base class for ls files.
+    """
+    instances = []
+    def __init__(self, path, set_colour=True):
+        self.__class__.instances.append(self)
+        self.path = path
+        self.fname = path.split('\\')[-1]
+        self.coloured_fname = _get_coloured_str(path)
+        self.set_colour = set_colour
+
+    def __repr__(self):
+        return self.coloured_fname if self.set_colour else self.fname
+
+    def __del__(self):
+        self.__class__.instances = []
+
+    @classmethod
+    def add_all(cls, joiner='\n'):
+        """
+        add every item in cls.instances, and return them joined
+        by joiner.
+        """
+        return joiner.join(map(lambda x: repr(x), cls.instances))
+
+    @classmethod
+    def clear(cls):
+        """
+        clear all instances from cls.
+        """
+        cls.instances = []
+
+    @classmethod
+    def sort(cls, showdot=True):
+        priority = {}
+        for key in _LS_COLOURS.keys():
+            priority[key] = []
+
+        for item in cls.instances:
+            filename = item.path
+            if os.path.islink(filename):
+                try:
+                    os.stat(filename)
+                    type_ = 'symlink'
+                except OSError:
+                    # the link is broken
+                    type_ = 'broken-symlink'
+
+            elif os.path.isdir(filename):
+                type_ = 'directory'
+
+            else:
+                # we are now relying on the extension
+                try:
+                    extension = filename.split('.')[-1]
+                except IndexError:
+                    # file has no extension
+                    extension = None
+                    type_ = 'other'
+
+                if extension is not None:
+                    if extension in _ARCHIVE_EXTENSIONS:
+                        type_ = 'archive'
+
+                    elif extension in _EXECUTABLE_EXTENSIONS:
+                        type_ = 'executable'
+
+                    elif extension == 'lnk':
+                        type_ = 'shortcut'
+
+                    else:
+                        type_ = 'other'
+            priority[type_].append(item)
+
+        string = ''
+        for type_ in PRIORITIES:
+            for item in priority[type_]:
+                if item.fname.startswith('.') and not showdot:
+                    continue
+                string += repr(item) + '\n'
+
+        return string
