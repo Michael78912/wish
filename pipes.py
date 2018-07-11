a2d72cf9e5f8f7ec436312a8ed37b890761eee6c
Michael Gill <michael8123@outlook.com>
2018-07-11 16:34:05 -0700

    started working on PipeHandler

--- pipes.py
+++ pipes.py
@@ -2,41 +2,65 @@
 pipes.py- a file for redirecting standard files
 """
 
+import re
+
+
 class _Pipe:
     pass
 
-class OutToIn(_Pipe):
-    """
-    redirects stdout to stdin
-    """
-    token = '|'
+class Pipes:
+    class OutToIn(_Pipe):
+        """
+        redirects stdout to stdin
+        """
+        token = '|'
 
-class OutToFile(_Pipe):
-    """
-    redirects stdout to file
-    """
-    token = '>'
+    class OutToFile(_Pipe):
+        """
+        redirects stdout to file
+        """
+        token = '>'
 
-class OutToFileA(_Pipe):
-    """
-    appends to file
-    """
-    token = '>>'
+    class OutToFileA(_Pipe):
+        """
+        appends to file
+        """
+        token = '>>'
 
-class And(_Pipe):
-    """
-    chains 2 commands
-    """
-    token = '&'
+    class And(_Pipe):
+        """
+        chains 2 commands
+        """
+        token = '&'
 
-class AndS(_Pipe):
-    """
-    does following commands only if previous is successfull
-    """
-    token = '&&'
+    class AndS(_Pipe):
+        """
+        does following commands only if previous is successfull
+        """
+        token = '&&'
 
-class Or(_Pipe):
+    class Or(_Pipe):
+        """
+        run next command only if previous fails
+        """
+        token = '||'
+
+
+class PipeHandler:
     """
-    run next command only if previous fails
+    can handle all of the pipes in a command.
     """
-    token = '||'
+    def __init__(self, cmdstr):
+        print(re.findall(r'''(?[\|&<>][^'"])[\|&<>]([\|&<>]:[^'"])''', cmdstr))
+        pipes = [x.strip() for x in re.findall(r'''[^'"][\|&><]{1}[^'"]|([<>\|&]|(<<|>>|&&|\|\|))''', cmdstr)]
+        cmds = [x.strip() for x in re.split(r'''[^'"][\|&><]{1}[^'"]''', cmdstr)]
+        print(pipes, cmds)
+
+        tokens = []
+        for i, p in enumerate(pipes):
+            tokens += [cmds[i], p]
+        tokens.append(cmds[i + 1])
+        print(tokens)
+
+
+PipeHandler('echo "howdy>hi" |tee tuna.txt > tuna.txt')
